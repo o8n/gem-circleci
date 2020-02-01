8ed0291e1c54a40c57591071b7613196b52423f0
okamotchan <m.okamotchan@gmail.com>
2020-02-01 18:05:44 +0900

    add .circleci/comfig.yml

--- spec/kinki_train_info_spec.rb
+++ spec/kinki_train_info_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe KinkiTrainInfo do
     expect(KinkiTrainInfo::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
